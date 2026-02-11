ba23dbb105915b5d7787ac0f0c3c5c98c5c332c2
K-Roon <66551757+K-Roon@users.noreply.github.com>
2026-02-11 20:12:00 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# deep_drill
+
+Deep Drill. 반복적으로 듣고 배우는 방법.
---
+++ android/app/src/main/java/com/kroon/deep_drill/MainActivity.java
@@ -0,0 +1,6 @@
+package com.kroon.deep_drill;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
