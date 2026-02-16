8a08f6f425afd90b0e850dc92a28080e39124563
K-Roon <66551757+K-Roon@users.noreply.github.com>
2026-02-16 22:59:00 +0900

    리버트 시켜야 할 듯,,,

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,23 +3,11 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-
   override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+      _ application: UIApplication,
+      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
-    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-
-    // 1) "liquid_glass_native" 뷰 타입 등록
-    controller
-      .register(
-        LiquidGlassFactory(
-          messenger: controller.binaryMessenger
-        ),
-        withId: "liquid_glass_native"
-      )
-
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
